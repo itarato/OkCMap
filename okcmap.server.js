c25555f824162d4374acb08a364d81e2a9f4b603
Peter Arato <it.arato@gmail.com>
2012-09-15 12:05:05 +0200

    Add new call receivers.

--- okcmap.server.js
+++ okcmap.server.js
@@ -3,7 +3,7 @@ console.log('Server init');
 var http = require('http');
 var url = require('url');
 var okcMapStorage = require('./okcmap.storage');
-var server = http.createServer(onServerCreated);
+var server = http.createServer(onRequestReceived);
 var collection = null;
 
 server.listen(8888, 'localhost');
@@ -13,13 +13,24 @@ okcMapStorage.openCollection(function(_collection) {
   console.log('Db is ready to use');
 });
 
-function onServerCreated(request, response) {
+function onRequestReceived(request, response) {
   console.log('Request in');
 
   var parsed_url = url.parse(request.url, true);
+
+  if (true) {
+    doMapDataResponse(response);
+  }
+  else {
+    doDataSave(response, parsed_url);
+  }
+}
+
+function doDataSave(response, parsed_url) {
   if (!parsed_url.query.hasOwnProperty('username')) {
     return;
   }
+
   collection.findOne({username: parsed_url.query.username}, function(err, item) {
     if (!item) {
       collection.insert(parsed_url.query);
@@ -28,4 +39,18 @@ function onServerCreated(request, response) {
 
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Request has been processed' + "\n");
+}
+
+function doMapDataResponse(response) {
+  collection.find(function(err, cursor){
+    cursor.each(function(err, item){
+      if (item) {
+        console.log(item);
+      }
+      else {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('hello');
+      }
+    });
+  });
 }
